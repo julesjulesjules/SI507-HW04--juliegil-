7550710fcdb3ef92a683cdf2957f3c46a536eeaf
Jules G <juliegil@umich.edu>
2018-01-29 17:03:20 -0500

    added list of answers + random choice setup

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,15 @@
 ### 1/29/2018
 ### Magic Eight Ball Function
 
+import random
+
 print("Welcome to the Magic Eight Ball game!")
 
-person_q = ("What is your question?")
+person_q = input("What is your question?")
+
+eightball_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+"You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+"Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+"Don't count on it", "My reply is no", "My sources say no", "Outlook is not so good", "Very doubtful"]
+
+print(random.choice(eightball_answers))
